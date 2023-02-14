047428ef0e840582a3343467f4458f8b35448699
ss-vainik <vainik.kakadiya@sculptsoft.com>
2023-02-14 09:55:49 +0530

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,134 +1,160 @@
-import { useState } from "react";
-import { Button, CardImg, Col, Form, Row } from "react-bootstrap";
-import Header from "./Components/Header";
+import { useEffect, useState } from "react";
 // import SOUND_MUTE from "./assets/images/sound-mute-alt.svg";
 // import SOUND_UNMUTE from "./assets/images/sound-max.svg";
 import DEFAULT from "./assets/images/default.jpg";
-import YOUTUBE_IMAGE from "./assets/images/youtube.jpg";
+import LOGO from "./assets/images/mt_logo.png";
 import { getId } from "./utils/general";
+import { Button, Grid, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const youtubeImageUrl = "https://img.youtube.com/vi/{{id}}/0.jpg";
 // const youtubeEmbed = "https://www.youtube.com/embed/{{id}}";
 
 function App() {
-  // const [youtubeLink, setYoutubeLink] = useState("");
-  // const [numberTab, setNumberTab] = useState("");
-  // // const [isMute, setIsMute] = useState(true);
-  // const [imageUrl, setImageUrl] = useState(YOUTUBE_IMAGE);
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-  // const handleClick = (e) => {
-  //   e.preventDefault();
-  //   if (numberTab > 20) {
-  //     alert("Please enter a below 20 tab");
-  //   } else {
-  //     if (youtubeLink) {
-  //       // const finalUrl =
-  //       //   youtubeEmbed.replace("{{id}}", getId(youtubeLink)) +
-  //       //   `?mute=${isMute ? 1 : 0}&autoplay=1`;
-  //       for (let i = 0; i < Number(numberTab); i++) {
-  //         window.open(
-  //           youtubeLink,
-  //           "hello" + i,
-  //           `width=200 height=200 left=${50 + 50 * i} top=${100 + 50 * i}`
-  //         );
-  //       }
-  //     } else {
-  //     }
-  //   }
-  // };
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const [numberTab, setNumberTab] = useState("");
+  const [errors, setError] = useState({});
+  // const [isMute, setIsMute] = useState(true);
+  const [imageUrl, setImageUrl] = useState();
 
-  // const handleYoutubeBlur = (e) => {
-  //   if (e.target.value) {
-  //     const finalUrl = youtubeImageUrl.replace("{{id}}", getId(e.target.value));
-  //     setImageUrl(finalUrl);
-  //   } else {
-  //     alert("please enter valid url");
-  //   }
-  // };
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (numberTab > 20) {
+      setError({ ...errors, number: "Please enter below 20" });
+    } else {
+      if (youtubeLink) {
+        // const finalUrl =
+        //   youtubeEmbed.replace("{{id}}", getId(youtubeLink)) +
+        //   `?mute=${isMute ? 1 : 0}&autoplay=1`;
+        for (let i = 0; i < Number(numberTab); i++) {
+          window.open(youtubeLink, "hello" + i, `width=200 height=200 left=${50 + 50 * i} top=${100 + 50 * i}`);
+        }
+      } else {
+      }
+    }
+  };
 
-  // return (
-  //   <>
-  //     <Header />
-  //     <div className="container">
-  //       <Row>
-  //         <Col md={8}>
-  //           <div className="card mt-4 p-4">
-  //             <Row>
-  //               <Col md={8}>
-  //                 <Form>
-  //                   <Form.Group className="mb-3" controlId="formBasicEmail">
-  //                     <Form.Label>Youtube link</Form.Label>
-  //                     <Form.Control
-  //                       type="text"
-  //                       placeholder="Past youtube link here"
-  //                       value={youtubeLink}
-  //                       onChange={(e) => setYoutubeLink(e.target.value)}
-  //                       onBlur={handleYoutubeBlur}
-  //                     />
-  //                     {/* <Form.Text className="text-muted">
-  //         We'll never share your email with anyone else.
-  //       </Form.Text> */}
-  //                   </Form.Group>
+  const handleYoutubeBlur = (e) => {
+    if (e.target.value) {
+      const finalUrl = youtubeImageUrl.replace("{{id}}", getId(e.target.value));
+      setImageUrl(finalUrl);
+    } else {
+      setError({ ...errors, link: "Please enter valid link" });
+      setImageUrl();
+    }
+  };
 
-  //                   <Form.Group className="mb-3" controlId="formBasicPassword">
-  //                     <Form.Label>Number of Tabs</Form.Label>
-  //                     <Form.Control
-  //                       type="number"
-  //                       placeholder="number of tabs"
-  //                       value={numberTab}
-  //                       onChange={(e) => setNumberTab(e.target.value)}
-  //                     />
-  //                   </Form.Group>
-  //                   {/* {isMute ? (
-  //                     <div
-  //                       className="sound-button"
-  //                       onClick={() => setIsMute(!isMute)}
-  //                     >
-  //                       <img src={SOUND_MUTE} alt="image1" />
-  //                       <div>Unmute</div>
-  //                     </div>
-  //                   ) : (
-  //                     <div
-  //                       className="sound-button"
-  //                       onClick={() => setIsMute(!isMute)}
-  //                     >
-  //                       <img src={SOUND_UNMUTE} alt="image2" />
-  //                       <div>Mute</div>
-  //                     </div>
-  //                   )} */}
-  //                   <Button
-  //                     variant="primary"
-  //                     type="submit"
-  //                     className="w-100"
-  //                     onClick={handleClick}
-  //                   >
-  //                     Submit
-  //                   </Button>
-  //                 </Form>
-  //               </Col>
-  //               <Col md={4}>
-  //                 <CardImg
-  //                   src={imageUrl}
-  //                   height={150}
-  //                   style={{ objectFit: "cover" }}
-  //                 ></CardImg>
-  //               </Col>
-  //             </Row>
-  //           </div>
-  //         </Col>
-  //         <Col md={4}>
-  //           <div className="card mt-4 p-4">hello</div>
-  //         </Col>
-  //       </Row>
-  //     </div>
-  //   </>
-  // );
+  const handleNumber = (e) => {
+    const value = e.target.value;
+    setNumberTab(value);
+    if (value > 20) {
+      setError({ ...errors, number: "Please enter a number" });
+    } else {
+      setError({ ...errors, number: "" });
+    }
+  };
+
+  const handleLink = (e) => {
+    const value = e.target.value;
+    setYoutubeLink(value);
+    if (value) {
+      setError({ ...errors, link: "" });
+    } else {
+      setImageUrl();
+    }
+  };
+
   return (
-    <div
-      style={{ backgroundImage: `url(${DEFAULT})`, height: "100vh" }}
-      className="main"
-    >
-      hello
-      <div class="cover black" data-color="black"></div>
+    <div style={{ backgroundImage: `url(${DEFAULT})`, height: "100vh" }} className="main">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <div className="card cardForm" style={{ zIndex: 999 }}>
+            <Box
+              sx={{
+                textAlign: "center",
+                mb: 3,
+                borderBottom: "1px solid #d4d7d9",
+              }}
+              className="cardForm1"
+            >
+              <img src={LOGO} alt="logo" width={80} height={50} style={{ objectFit: "cover" }} />
+              {/* <Typography variant="subtitle1" sx={{ fontWeight: 500 }}>
+                Tab Multiplier
+              </Typography> */}
+            </Box>
+
+            <Box className="cardForm1">
+              <TextField
+                id="standard-basic"
+                label="Tab Link"
+                variant="standard"
+                value={youtubeLink}
+                sx={{ mb: 2 }}
+                fullWidth
+                onChange={handleLink}
+                onBlur={handleYoutubeBlur}
+                error={errors?.link}
+                helperText={errors?.link}
+              />
+              {getId(youtubeLink) && imageUrl && (
+                <Box data-aos="zoom-in">
+                  <img src={imageUrl} width={"100%"} height="125px" style={{ objectFit: "cover" }} alt="thumbnail" />
+                </Box>
+              )}
+              {imageUrl && (
+                <Box>
+                  <TextField
+                    id="standard-basic"
+                    label="Number of Tabs"
+                    variant="standard"
+                    value={numberTab}
+                    type="number"
+                    fullWidth
+                    onChange={handleNumber}
+                    error={errors?.number}
+                    helperText={errors?.number}
+                  />
+                </Box>
+              )}
+
+              {/* {isMute ? (
+                      <div
+                        className="sound-button"
+                        onClick={() => setIsMute(!isMute)}
+                      >
+                        <img src={SOUND_MUTE} alt="image1" />
+                        <div>Unmute</div>
+                      </div>
+                    ) : (
+                      <div
+                        className="sound-button"
+                        onClick={() => setIsMute(!isMute)}
+                      >
+                        <img src={SOUND_UNMUTE} alt="image2" />
+                        <div>Mute</div>
+                      </div>
+                    )} */}
+              <div style={{ margin: "auto" }} className="text-center">
+                <Button
+                  sx={{ mt: 4, borderRadius: "20px" }}
+                  variant="contained"
+                  disabled={youtubeLink ? false : true}
+                  onClick={handleClick}
+                >
+                  Submit
+                </Button>
+              </div>
+            </Box>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6} md={8} lg={9}></Grid>
+      </Grid>
+      <div className="cover black" data-color="black"></div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
